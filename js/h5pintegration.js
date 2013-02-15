71e4722ab669d5cc6afd4583dfd199345004aa44
Frode Petterson <frode.petterson@amendor.com>
2013-02-15 14:32:45 +0100

    Prepared for alpha2.

--- js/h5pintegration.js
+++
@@ -1,9 +0,0 @@
-var H5PIntegration = H5PIntegration || {};
-
-H5PIntegration.getJsonContent = function (contentId) {
-  return Drupal.settings.h5p.jsonContent[contentId];
-};
-
-H5PIntegration.getContentPath = function (contentId) {
-  return Drupal.settings.h5p.jsonContentPath + contentId + '/';
-};
